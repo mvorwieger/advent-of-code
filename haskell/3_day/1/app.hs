224995acf9c0acd425c1b82ab61dbce51d7c6b8a
mvorwieger <michel.vorwieger@outlook.de>
2018-12-12 18:34:29 +0100

    generateFigure

--- haskell/3_day/1/app.hs
+++ haskell/3_day/1/app.hs
@@ -16,17 +16,24 @@ exampleGrid = [
 insert :: Id -> Cord -> Size -> Grid -> Grid
 insert i c s = undefined
 
+-- map impl. that passes index as second arg.
+mapInd :: (a -> Int -> b) -> [a] -> [b]
+mapInd f l = zipWith f l [0..]
+
 -- replicate :: Int -> a -> [a]
 -- zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
 -- TODO: Look for 2D Vector Addition ?
 -- calcCords :: Cord -> Size -> [Field]
-calcCords (x, y) (h, w) = let 
+calcCords :: Field -> Cord -> Size -> Grid
+calcCords i (x, y) (h, w) = let 
         gridWidth    = x + w
         gridHeight  = y + h
         body        = replicate (gridHeight) $ replicate (gridWidth) Empty 
-        rangeX      = (x, x + w)
-        rangeY      = (y, y + h)
-            in body 
+        rangeX      = [x .. gridWidth]
+        rangeY      = [y .. gridHeight]
+            in mapInd (\row y -> mapInd (\cell x -> if (x `elem` rangeX) && (y `elem` rangeY) then i else cell ) row) body 
+
+
 
 countOverlappingRow :: Row -> Int
 countOverlappingRow r = length $ filter (==Overlapping) r
