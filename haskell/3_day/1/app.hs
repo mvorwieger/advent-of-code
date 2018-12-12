45e755c02a5f497c2313c303d2da9e93818a3d26
mvorwieger <michel.vorwieger@outlook.de>
2018-12-12 17:32:58 +0100

    added count functions

--- haskell/3_day/1/app.hs
+++ haskell/3_day/1/app.hs
@@ -1,4 +1,17 @@
-data Field = Empty | Cell Int | Overlapping deriving (Show, Eq)
+import Data.List
 
+data Field = Empty | Cell Int | Overlapping deriving (Show, Eq)
 type Row = [Field]
 type Grid = [Row]
+
+exampleGrid = [
+      [Empty, Cell 5, Overlapping]
+    , [Cell 2, Cell 5, Overlapping]
+    , [Empty, Empty, Overlapping]
+    ]
+
+countOverlappingRow :: Row -> Int
+countOverlappingRow r = length $ filter (==Overlapping) r
+
+countOverlappingGrid :: Grid -> Int
+countOverlappingGrid g = foldl (\acc r -> (+) acc $ countOverlappingRow r) 0 g 
