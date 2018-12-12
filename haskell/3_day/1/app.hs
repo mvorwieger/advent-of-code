5a0c552008a7c5dfe538f85bac13220fdf410abe
mvorwieger <michel.vorwieger@outlook.de>
2018-12-12 17:35:12 +0100

    cord size types

--- haskell/3_day/1/app.hs
+++ haskell/3_day/1/app.hs
@@ -3,12 +3,18 @@ import Data.List
 data Field = Empty | Cell Int | Overlapping deriving (Show, Eq)
 type Row = [Field]
 type Grid = [Row]
+type Cord = (Int, Int)
+type Size = (Int, Int)
+type Id = Int
 
 exampleGrid = [
       [Empty, Cell 5, Overlapping]
     , [Cell 2, Cell 5, Overlapping]
     , [Empty, Empty, Overlapping]
     ]
+
+insert :: Id -> Cord -> Size
+insert = undefined
 
 countOverlappingRow :: Row -> Int
 countOverlappingRow r = length $ filter (==Overlapping) r
