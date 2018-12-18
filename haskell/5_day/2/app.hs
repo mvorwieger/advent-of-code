a9003ae3990429f3db6dd8cf51077640d88fd8c7
mvorwieger <michel.vorwieger@outlook.de>
2018-12-18 20:15:52 +0100

    day5/part2

--- haskell/5_day/2/app.hs
+++ haskell/5_day/2/app.hs
@@ -10,7 +10,7 @@ main = do x <- readFile "input.txt"
           print . show . exec $ x
 
 exec :: String -> Int
-exec = length . collapse 
+exec = shortestPathByRemovingOneChar 
 
 doesReact :: Char -> Char -> Bool
 doesReact a b = a /= b && (a == toUpper b || b == toUpper a)
@@ -24,3 +24,12 @@ collapse :: Input -> Input
 collapse s | s == s' = s
            | otherwise = collapse s'
     where s' = next s
+
+compareLetter :: Char -> Char -> Ordering
+compareLetter a b = toUpper a `compare` toUpper b
+
+sameLetter :: Char -> Char -> Bool
+sameLetter a b = toUpper a == toUpper b
+
+shortestPathByRemovingOneChar :: String -> Int
+shortestPathByRemovingOneChar i = minimum . map (\a -> exec (filter (not . sameLetter (head a)) i)) . groupBy sameLetter . sortBy compareLetter $ i
